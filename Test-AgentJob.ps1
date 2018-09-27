a5d4384df0412dbc100ccf36e6b4d2393d780bc1
Adarsh <cadarsh88@gmail.com>
2018-09-27 19:17:09 +0530

    Create Test-AgentJob.ps1

---
+++ Test-AgentJob.ps1
@@ -0,0 +1,55 @@
+Function Test-AgentJob{
+    [CmdletBinding()]
+    param
+    (
+        [ValidateNotNull()]
+        [System.String]
+        $SQLServer = $env:COMPUTERNAME,
+
+        [ValidateNotNull()]
+        [System.String]
+        $SQLInstanceName = 'MSSQLSERVER',
+
+        [Parameter()]
+        [ValidateSet('WindowsUser', 'SqlLogin')]
+        [System.String]
+        $LoginType = 'WindowsUser',
+
+        [ValidateNotNull()]
+        [System.String]
+        $AgentJobName,
+
+        [ValidateNotNull()]
+        [ValidateSet('Status', 'Start', 'Stop')]
+        [System.String]
+        $Action = "Status"
+    )
+
+    try{
+        $sqlconnection = Connect-SQL -SQLServer $SQLServer -SQLInstanceName $SQLInstanceName -LoginType $LoginType
+        $jobobj = $sqlconnection.JobServer.Jobs | Where-Object {$_.Name -eq $AgentJobName}
+        if($Action -eq "Status"){
+            $ActionStatus = $jobobj.CurrentRunStatus
+            Write-Verbose "Current Job Status: $ActionStatus" -Verbose
+        } elseif ($Action -eq "Start") {
+            $jobobj.Start()
+            $ActionStatus = "Started"
+            Write-Verbose "Job $AgentJobName started" -Verbose
+        } elseif($Action -eq "Stop") {
+            $jobobj.Stop()
+            $ActionStatus = "Stopped"
+            Write-Verbose "Job $AgentJobName stopped" -Verbose
+        }
+        return New-Object PSObject -Property @{
+            Status=$ActionStatus
+            Error=$null
+		}
+    } Catch {
+        $Exception= $_.Exception.Message	
+        Write-Host "Exception Occured: $Exception"
+        return New-Object PSObject -Property @{
+            Status=$null
+            Error=$Exception
+		}
+    }
+}
